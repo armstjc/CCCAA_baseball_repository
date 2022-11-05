6c4b2cb7040634dac5c9a51992e3a50a37aed1e3
Joseph Armstrong <armstjc@mail.uc.edu>
2022-11-05 19:29:49 -0400

    11/05/2022 Stats Update 5
    5400/17220 (31.3%) of CCCAA player game logs are downloaded.

--- get_cccaa_game_stats.py
+++ get_cccaa_game_stats.py
@@ -23,7 +23,7 @@ def get_CCCAA_gamelogs(teams_df:pd.DataFrame()):
 	player_count = len(player_urls)
 	# driver = webdriver.Chrome(
 	# 	executable_path=webdriverPath)
-	count = 5200
+	count = 5400
 	for i in tqdm(range(count,player_count)):
 		
 		print(f'{count}/{player_count}')
