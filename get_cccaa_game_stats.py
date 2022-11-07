140a4d5034706e2fa1d4284ebf452c34b86dae11
Joseph Armstrong <armstjc@mail.uc.edu>
2022-11-07 15:14:16 -0500

    11/07/2022 Stats Update

--- get_cccaa_game_stats.py
+++ get_cccaa_game_stats.py
@@ -23,7 +23,7 @@ def get_CCCAA_gamelogs(teams_df:pd.DataFrame()):
 	player_count = len(player_urls)
 	# driver = webdriver.Chrome(
 	# 	executable_path=webdriverPath)
-	count = 12800
+	count = 15400
 	for i in tqdm(range(count,player_count)):
 		
 		print(f'{count}/{player_count}')
