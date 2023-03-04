3a087a3e21d0ab8a1a47747e1d917b9202af220e
Joseph Armstrong <armstjc@mail.uc.edu>
2023-03-03 21:38:38 -0500

    CCCAA Baseball Stats Update

--- get_cccaa_game_stats.py
+++ get_cccaa_game_stats.py
@@ -22,7 +22,7 @@ def get_CCCAA_gamelogs(teams_df:pd.DataFrame()):
 	player_count = len(player_urls)
 	# driver = webdriver.Chrome(
 	# 	executable_path=webdriverPath)
-	count = 1340
+	count = 0
 	for i in tqdm(range(count,player_count)):
 		
 		print(f'{count}/{player_count}')
